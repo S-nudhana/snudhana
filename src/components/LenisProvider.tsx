3b1b216001ed0663652c3ffdcfc1008e7e19cca3
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-06-12 20:30:19 +0700

    test

--- src/components/LenisProvider.tsx
+++ src/components/LenisProvider.tsx
@@ -10,8 +10,6 @@ export default function LenisProvider({ children }: { children: React.ReactNode 
         lerp: 0.08,
         duration: 0.7,
         easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-        smooth: true,
-        direction: 'vertical',
       }}
     >
       {children}
