da744dab7009777a2486db2354658d9554099479
Nudhana Sarutipaisan <nu37317@gmail.com>
2025-07-09 09:12:38 +0700

    bug/button

--- src/app/cert/page.tsx
+++ src/app/cert/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Suspense } from 'react'
 import { useSearchParams } from 'next/navigation'
 import Image from "next/image"
 
@@ -24,33 +24,38 @@ export default function CertPage() {
     }, [searchParams])
 
     return (
-        <div className="md:ml-[260px] text-lightGray flex flex-col lg:justify-start gap-3 lg:gap-7 w-full min-h-[70vh]">
-            <BackBTN />
-            <h1 className="text-3xl font-bold text-yellow">
-                {data?.title} Certificates
-            </h1>
-            <p className='text-gray'>{data?.longDescription}</p>
-            <div className="flex flex-col md:flex-row flex-wrap gap-6">
-                {data?.link && data.link.map((item, index) => (
-                    <div key={index} className="flex flex-col items-center justify-center pt-4 rounded-xl">
-                        <Image
-                            src={item.img}
-                            alt={item.text}
-                            width={300}
-                            height={200}
-                            className="rounded-lg"
-                        />
-                        <a
-                            href={item.url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="mt-2 text-gray hover:underline underline-offset-2 cursor-none"
-                        >
-                            {item.text}
-                        </a>
-                    </div>
-                ))}
+        <Suspense fallback={
+            <div className="md:ml-[260px] text-lightGray flex lg:justify-start gap-3 lg:gap-7 w-full min-h-[70vh]">
+                <div className="text-gray flex align-middle justify-center">Loading...</div>
+            </div>}>
+            <div className="md:ml-[260px] text-lightGray flex flex-col lg:justify-start gap-3 lg:gap-7 w-full min-h-[70vh]">
+                <BackBTN />
+                <h1 className="text-3xl font-bold text-yellow">
+                    {data?.title} Certificates
+                </h1>
+                <p className='text-gray'>{data?.longDescription}</p>
+                <div className="flex flex-col md:flex-row flex-wrap gap-6">
+                    {data?.link && data.link.map((item, index) => (
+                        <div key={index} className="flex flex-col items-center justify-center pt-4 rounded-xl">
+                            <Image
+                                src={item.img}
+                                alt={item.text}
+                                width={300}
+                                height={200}
+                                className="rounded-lg"
+                            />
+                            <a
+                                href={item.url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="mt-2 text-gray hover:underline underline-offset-2 cursor-none"
+                            >
+                                {item.text}
+                            </a>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
+        </Suspense>
     )
 }
